5992f55aa19ee7976818ba2905a27b043090f05b
Phil Thompson <phil@riverbankcomputing.com>
2025-07-26 18:10:25 +0100

    Added the simple class test.

---
+++ test/classes/test_classes.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Copyright (c) 2025 Phil Thompson <phil@riverbankcomputing.com>
+
+
+from utils import SIPTestCase
+
+
+class ClassesTestCase(SIPTestCase):
+    """ Test the support for classes. """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Set up the test case. """
+
+        super().setUpClass()
+
+        import classes_module as c_mod
+
+        cls.c_mod = c_mod
+
+    def test_simple_classes(self):
+        """ Test the support simple classes. """
+
+        self.assertIsInstance(self.c_mod.Klass(), self.c_mod.Klass)
