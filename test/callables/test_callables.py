3250c6f21d82e1a607cb09ab7acbedc60aa267ae
Phil Thompson <phil@riverbankcomputing.com>
2025-07-26 15:07:28 +0100

    Added tests for the /KeywordArgs/ annotation.

--- test/callables/test_callables.py
+++ test/callables/test_callables.py
@@ -50,3 +50,53 @@ class CallablesTestCase(SIPTestCase):
         self.assertEqual(len(res), 2)
         self.assertEqual(res[0], 1)
         self.assertEqual(res[1], 2)
+
+    def test_insufficient_args(self):
+        """ Test insufficient arguments. """
+
+        self.assertRaises(TypeError, self.c_mod.module_doubler, (), {})
+
+    def test_excessive_args(self):
+        """ Test excessive arguments. """
+
+        self.assertRaises(TypeError, self.c_mod.module_doubler, (1, 1), {})
+
+    def test_wrong_type_args(self):
+        """ Test incorrect argument types. """
+
+        self.assertRaises(TypeError, self.c_mod.module_doubler, ('1',), {})
+
+    def test_no_kwd_args_support(self):
+        """ Test the lack of support for keyword arguments. """
+
+        self.assertRaises(TypeError, self.c_mod.module_doubler, (),
+                dict(value=3))
+
+    def test_bad_kwd_args(self):
+        """ Test bad keyword arguments. """
+
+        self.assertRaises(TypeError, self.c_mod.incr_optional_kwd_args, (1, ),
+                dict(bad=3))
+        self.assertRaises(TypeError, self.c_mod.incr_optional_kwd_args, (1, 2),
+                dict(incr=3))
+
+    def test_optional_kwd_args_support(self):
+        """ Test the support for optional keyword arguments. """
+
+        self.assertEqual(self.c_mod.incr_optional_kwd_args(1), 2)
+        self.assertEqual(self.c_mod.incr_optional_kwd_args(1, 2), 3)
+        self.assertEqual(self.c_mod.incr_optional_kwd_args(1, incr=3), 4)
+
+    def test_all_kwd_args_support(self):
+        """ Test the support for all keyword arguments. """
+
+        self.assertEqual(self.c_mod.incr_all_kwd_args(1), 2)
+        self.assertEqual(self.c_mod.incr_all_kwd_args(1, 2), 3)
+        self.assertEqual(self.c_mod.incr_all_kwd_args(1, incr=3), 4)
+
+        self.assertEqual(self.c_mod.incr_all_kwd_args(value=1), 2)
+        self.assertEqual(self.c_mod.incr_all_kwd_args(value=1, incr=2), 3)
+        self.assertEqual(self.c_mod.incr_all_kwd_args(incr=3, value=1), 4)
+
+        self.assertRaises(TypeError, self.c_mod.incr_all_kwd_args, (),
+                dict(incr=3))
