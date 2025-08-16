317b4e8a5648e777ec5329b2db285ca3206cef99
Phil Thompson <phil@riverbankcomputing.com>
2025-08-16 22:48:05 +0100

    Refactored the standalone sip module tests to handle module and type names.

--- test/standalone_sip_module/test_standalone_sip_module.py
+++ test/standalone_sip_module/test_standalone_sip_module.py
@@ -9,9 +9,42 @@ from utils import SIPTestCase
 class StandaloneSipModuleTestCase(SIPTestCase):
     """ Test the support for a standalone (ie. imported) sip module. """
 
+    namespace = 'ns'
     use_sip_module = True
 
-    def test_standalone_sip_module(self):
-        """ Test the support importing a standalone sip module. """
+    @classmethod
+    def setUpClass(cls):
+        """ Set up the test case. """
 
-        import standalone_sip_module_module
+        super().setUpClass()
+
+        import ns.standalone_sip_module_module as w_mod
+        import ns.sip as s_mod
+
+        cls.w_mod = w_mod
+        cls.s_mod = s_mod
+
+    def test_wrapped_module_name(self):
+        """ Test the support for a wrapped module's __name__. """
+
+        self.assertEqual(self.w_mod.__name__,
+                'ns.standalone_sip_module_module')
+
+    def test_toplevel_wrapped_types(self):
+        """ Test the support for toplevel wrapped types. """
+
+        self.assertEqual(self.w_mod.Klass.__module__,
+                'ns.standalone_sip_module_module')
+        self.assertEqual(self.w_mod.Klass.__name__, 'Klass')
+        self.assertEqual(self.w_mod.Klass.__qualname__, 'Klass')
+
+    def test_nested_wrapped_types(self):
+        """ Test the support for nested wrapped types. """
+
+        print()
+        print("!!! Klass.Nested.__module__:", self.w_mod.Klass.Nested.__module__)
+        print("!!! Klass.Nested.__name__:", self.w_mod.Klass.Nested.__name__)
+        print("!!! Klass.Nested.__qualname__:", self.w_mod.Klass.Nested.__qualname__)
+        #self.assertEqual(self.w_mod.Klass.Nested.__module__, 'ns.standalone_sip_module_module')
+        self.assertEqual(self.w_mod.Klass.Nested.__name__, 'Nested')
+        #self.assertEqual(self.w_mod.Klass.Nested.__qualname__, 'Klass.Nested')
