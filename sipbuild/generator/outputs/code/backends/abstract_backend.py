0b382ada8747814210ff21392c3dd2f9d1e92c39
Phil Thompson <phil@riverbankcomputing.com>
2025-11-12 15:50:14 +0000

    Refactored the code generation to implement ABI version-specific backends.

---
+++ sipbuild/generator/outputs/code/backends/abstract_backend.py
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Copyright (c) 2025 Phil Thompson <phil@riverbankcomputing.com>
+
+
+from abc import ABC, abstractmethod
+
+
+class AbstractBackend(ABC):
+    """ The abstract base class for backend code generators. """
+
+    def __init__(self, spec):
+        """ Initialise the backend. """
+
+        self.spec = spec
+
+    @staticmethod
+    def factory(spec):
+        """ Return an appropriate backend for the target ABI. """
+
+        from .v12v13 import v12v13Backend as backend
+
+        return backend(spec)
+
+    @abstractmethod
+    def g_composite_module_code(self, sf, py_debug):
+        """ Generate the code for a composite module. """
+
+        ...
+
+    @abstractmethod
+    def g_iface_file_code(self, sf, bindings, project, py_debug, buildable,
+            iface_file, need_postinc):
+        """ Generate the code specific to an interface file. """
+
+        ...
+
+    @abstractmethod
+    def g_module_code(self, sf, bindings, project, py_debug, buildable):
+        """ Generate the code for a module excluding the code specific to an
+        interface file.  It returns a closure that will be passed to
+        g_module_header_file().
+        """
+
+        ...
+
+    @abstractmethod
+    def g_module_header_file(self, sf, bindings, py_debug, closure):
+        """ Generate the internal module API header file. """
+
+        ...
