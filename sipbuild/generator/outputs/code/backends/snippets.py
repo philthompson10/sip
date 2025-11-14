4c28e1a9cb0ffc3652f3df35c08810fa21b9b280
Phil Thompson <phil@riverbankcomputing.com>
2025-11-14 15:41:16 +0000

    Refactored the generation of a module's API file.

--- sipbuild/generator/outputs/code/backends/snippets.py
+++ sipbuild/generator/outputs/code/backends/snippets.py
@@ -3,14 +3,17 @@
 # Copyright (c) 2025 Phil Thompson <phil@riverbankcomputing.com>
 
 
+from ....specification import QualifierType
+
+
 def g_composite_module_code(sf, py_debug, backend):
     """ Generate the implementation of a composite module. """
 
     spec = backend.spec
     module = spec.module
 
-    g_declare_limited_api(sf, py_debug)
-    g_include_sip_h(sf, module)
+    _declare_limited_api(sf, py_debug)
+    _include_sip_h(sf, module)
 
     sf.write(
 '''
@@ -64,34 +67,81 @@ f'    sip_import_component_module(sipModuleDict, "{mod.fq_py_name}");\n')
 ''')
 
 
-def g_declare_limited_api(sf, py_debug, module=None):
-    """ Declare the use of the limited API. """
+def g_internal_api_header(sf, bindings, py_debug, closure, backend):
+    """ Generate the C++ internal module API header file. """
 
-    if py_debug:
-        return
+    spec = backend.spec
+    module = spec.module
+    module_name = spec.module.py_name
 
-    if module is None or module.use_limited_api:
-        sf.write(
-'''
-#if !defined(Py_LIMITED_API)
-#define Py_LIMITED_API
-#endif
-''')
-
-
-def g_include_sip_h(sf, module):
-    """ Generate the inclusion of sip.h. """
-
-    if module.py_ssize_t_clean:
-        sf.write(
-'''
-#define PY_SSIZE_T_CLEAN
-''')
-
+    # The include files.
     sf.write(
-'''
-#include "sip.h"
+f'''#ifndef _{module_name}API_H
+#define _{module_name}API_H
 ''')
+
+    _declare_limited_api(sf, py_debug, module=module)
+    _include_sip_h(sf, module)
+
+    if pyqt5_supported(spec) or pyqt6_supported(spec):
+        sf.write(
+'''
+#include <QMetaType>
+#include <QThread>
+''')
+
+    # Define the qualifiers.
+    qualifier_defines = []
+
+    _append_qualifier_defines(module, bindings, qualifier_defines)
+
+    for imported_module in module.all_imports:
+        _append_qualifier_defines(imported_module, bindings, qualifier_defines)
+
+    if len(qualifier_defines) != 0:
+        sf.write('\n/* These are the qualifiers that are enabled. */\n')
+
+        for qualifier_define in qualifier_defines:
+            sf.write(qualifier_define + '\n')
+
+        sf.write('\n')
+
+    # Handle the module closure.
+    backend.handle_module_closure(sf, closure)
+
+    # Generate the SIP API.
+    backend.g_sip_api(sf)
+
+    # Generate the module's API.
+    backend.g_module_api(sf, bindings)
+
+    for imported_module in module.all_imports:
+        backend.g_imported_module_api(sf, imported_module)
+
+    if pyqt5_supported(spec) or pyqt6_supported(spec):
+        sf.write(
+f'''
+typedef const QMetaObject *(*sip_qt_metaobject_func)(sipSimpleWrapper *, sipTypeDef *);
+extern sip_qt_metaobject_func sip_{module_name}_qt_metaobject;
+
+typedef int (*sip_qt_metacall_func)(sipSimpleWrapper *, sipTypeDef *, QMetaObject::Call, int, void **);
+extern sip_qt_metacall_func sip_{module_name}_qt_metacall;
+
+typedef bool (*sip_qt_metacast_func)(sipSimpleWrapper *, const sipTypeDef *, const char *, void **);
+extern sip_qt_metacast_func sip_{module_name}_qt_metacast;
+''')
+
+    # Handwritten code.
+    sf.write_code(spec.exported_header_code)
+    sf.write_code(module.module_header_code)
+
+    # Make sure any header code needed by the default exception is included.
+    if module.default_exception is not None:
+        sf.write_code(module.default_exception.iface_file.type_header_code)
+
+    # Note that we don't forward declare the virtual handlers.  This is because
+    # we would need to #include everything needed for their argument types.
+    sf.write('\n#endif\n')
 
 
 def g_module_docstring(sf, module):
@@ -127,3 +177,79 @@ PyMODINIT_FUNC PyInit_{module_name}({arg_type})
 #endif
 {{
 ''')
+
+
+def pyqt5_supported(spec):
+    """ Return True if the PyQt5 plugin was specified. """
+
+    return 'PyQt5' in spec.plugins
+
+
+def pyqt6_supported(spec):
+    """ Return True if the PyQt6 plugin was specified. """
+
+    return 'PyQt6' in spec.plugins
+
+
+def _append_qualifier_defines(module, bindings, qualifier_defines):
+    """ Append the #defines for each feature defined in a module to a list of
+    them.
+    """
+
+    for qualifier in module.qualifiers:
+        qualifier_type_name = None
+
+        if qualifier.type is QualifierType.TIME:
+            if _qualifier_enabled(qualifier, bindings):
+                qualifier_type_name = 'TIMELINE'
+
+        elif qualifier.type is QualifierType.PLATFORM:
+            if _qualifier_enabled(qualifier, bindings):
+                qualifier_type_name = 'PLATFORM'
+
+        elif qualifier.type is QualifierType.FEATURE:
+            if qualifier.name not in bindings.disabled_features and qualifier.enabled_by_default:
+                qualifier_type_name = 'FEATURE'
+
+        if qualifier_type_name is not None:
+            qualifier_defines.append(f'#define SIP_{qualifier_type_name}_{qualifier.name}')
+
+
+def _declare_limited_api(sf, py_debug, module=None):
+    """ Declare the use of the limited API. """
+
+    if py_debug:
+        return
+
+    if module is None or module.use_limited_api:
+        sf.write(
+'''
+#if !defined(Py_LIMITED_API)
+#define Py_LIMITED_API
+#endif
+''')
+
+
+def _include_sip_h(sf, module):
+    """ Generate the inclusion of sip.h. """
+
+    if module.py_ssize_t_clean:
+        sf.write(
+'''
+#define PY_SSIZE_T_CLEAN
+''')
+
+    sf.write(
+'''
+#include "sip.h"
+''')
+
+
+def _qualifier_enabled(qualifier, bindings):
+    """ Return True if a qualifier is enabled. """
+
+    for tag in bindings.tags:
+        if qualifier.name == tag:
+            return qualifier.enabled_by_default
+
+    return False
