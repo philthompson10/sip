695d46cc0ed9e60655fcea0777d45572e1517c5b
Phil Thompson <phil@riverbankcomputing.com>
2026-01-23 16:37:35 +0000

    Fixed a regression in the support for namespace attributes.

--- sipbuild/module/source/14/0/sip_module_wrapper.c
+++ sipbuild/module/source/14/0/sip_module_wrapper.c
@@ -954,7 +954,7 @@ static int compare_type_nr(const void *key, const void *el,
     const sipTypeSpec *ts = m_spec->type_specs[type_nr];
     const char *s2;
 
-    if (sipTypeIsClass(ts))
+    if (sipTypeIsClass(ts) || sipTypeIsNamespace(ts))
         s2 = strrchr(((const sipClassTypeSpec *)ts)->container.fq_py_name, '.') + 1;
     else
         s2 = ((const sipEnumTypeSpec *)ts)->py_name;
